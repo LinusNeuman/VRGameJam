2c5aa91f101e58521d62858a89757e6c2ff6a770
Daaaavid <db.dregel@gmail.com>
2016-11-12 18:16:06 +0100

    created cannonBillboard script

---
+++ PirateStress/Assets/Scripts/cannonBillboard.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class cannonBillboard : MonoBehaviour
+{
+    [SerializeField]
+    private bool isWaiting;
+
+    [SerializeField]
+    private Image myImage;
+
+    [SerializeField]
+    private Sprite arrow;
+
+    [SerializeField]
+    private Sprite check;
+
+	// Use this for initialization
+	void Start ()
+    {
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(isWaiting == true)
+        {
+            Color col;
+
+            col.r = 255.0f;
+            col.g = 0.0f;
+            col.b = 0.0f;
+            col.a = 255.0f;
+
+            myImage.color = col;
+            myImage.sprite = arrow;
+        }
+        else if(isWaiting == false)
+        {
+            Color col;
+
+            col.r = 0.0f;
+            col.g = 255.0f;
+            col.b = 0.0f;
+            col.a = 255.0f;
+
+            myImage.color = col;
+            myImage.sprite = check;
+        }
+	}
+}
