9a13f13bf0f39353c9c0088dcccdefd09afce27b
HERMODS\stdano1 <dannerune@hotmail.se>
2016-11-12 19:29:14 +0100

    Updated cannon

--- PirateStress/Assets/CannonScript.cs
+++ PirateStress/Assets/CannonScript.cs
@@ -3,8 +3,22 @@ using System.Collections;
 
 public class CannonScript : MonoBehaviour
 {
-
     private bool myIsActive = false;
 
+    private Animator myAnimator;
 
+    void Awake()
+    {
+        myAnimator = GetComponent<Animator>();
+    }
+
+    public void Reload()
+    {
+        myAnimator.SetBool("IsReloading", true);
+    }
+
+    public void Shot()
+    {
+        myAnimator.SetBool("IsReloading", false);
+    }
 }
