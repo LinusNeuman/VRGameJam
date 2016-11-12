1b3f519c3641bd223a878941a564440d04663c5a
Linus Neuman <linus.neuman.sp15@thegameassembly.com>
2016-11-12 17:49:51 +0100

    Cannonball and gunpowder logic

---
+++ PirateStress/Assets/GunpowderLogic.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GunpowderLogic : MonoBehaviour {
+    public enum ePowderType
+    {
+        Normal,
+        HighExplosive
+    }
+
+    [SerializeField]
+    private ePowderType myPowderType = ePowderType.Normal;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public ePowderType GetPowderType()
+    {
+        return myPowderType;
+    }
+}
