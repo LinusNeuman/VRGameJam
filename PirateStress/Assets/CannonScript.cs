f440df6d72b43cdad57107075daa32a922889366
HERMODS\stdano1 <dannerune@hotmail.se>
2016-11-12 21:37:45 +0100

    Cannon fixes.

--- PirateStress/Assets/CannonScript.cs
+++ PirateStress/Assets/CannonScript.cs
@@ -8,14 +8,26 @@ public class CannonScript : MonoBehaviour
 
     private Animator myAnimator;
     private Rigidbody myCannonShot;
+    private float myCooldownTimer = 0;
 
     void Awake()
     {
         myAnimator = GetComponent<Animator>();
     }
 
+    void Update()
+    {
+        myCooldownTimer -= Time.deltaTime;
+        myCooldownTimer = Mathf.Max(0, myCooldownTimer);
+    }
+
     public void Reload()
     {
+        if (myCooldownTimer > 0.0f)
+        {
+            return;
+        }
+
         if (myCannonShot != null)
         {
             myCannonShot.AddForce(-32, 0, 0, ForceMode.Impulse);
@@ -26,6 +38,7 @@ public class CannonScript : MonoBehaviour
     public void Shot(Collider aCollider)
     {
         myCannonShot = aCollider.GetComponent<Rigidbody>();
+        myCooldownTimer = 1.5f;
         myAnimator.SetBool("IsReloading", false);
     }
 
