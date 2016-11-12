86f43ae05fec4e5c3354c4d579fc3478b9c0d0c8
Daaaavid <db.dregel@gmail.com>
2016-11-12 16:00:41 +0100

    Created face player and oscillate scripts

---
+++ PirateStress/Assets/facePlayer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class facePlayer : MonoBehaviour {
+
+    [SerializeField]
+    private Camera playerEyes;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        gameObject.transform.LookAt(playerEyes.transform.position);
+	}
+}
