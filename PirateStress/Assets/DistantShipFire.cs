b76b83a3655014eb9362b80ca1d847813a3cf728
Linus Neuman <linus.neuman.sp15@thegameassembly.com>
2016-11-13 13:07:38 +0100

    Plays distant sound and also plays sound for cannon

---
+++ PirateStress/Assets/DistantShipFire.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DistantShipFire : MonoBehaviour {
+
+    private float myTimerSinceShooting = 0.0f;
+
+    [SerializeField]
+    private float myFireCooldown = 0.0f;
+    [SerializeField]
+    private float myFireRandomAmount = 0.0f;
+
+    private float myFireMaxTime = 0.0f;
+	// Use this for initialization
+	void Start () {
+        myFireMaxTime = Random.Range(myFireCooldown - myFireRandomAmount, myFireCooldown + myFireRandomAmount);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        myTimerSinceShooting += Time.deltaTime;	
+
+        if(myTimerSinceShooting >= myFireMaxTime)
+        {
+            myTimerSinceShooting = 0.0f;
+            myFireMaxTime = Random.Range(myFireCooldown - myFireRandomAmount, myFireCooldown + myFireRandomAmount);
+
+            // shoot
+            Fire();
+        }
+	}
+
+    void Fire()
+    {
+        transform.FindChild("SoundEffects").GetComponent<AudioSource>().Play();
+    }
+}
